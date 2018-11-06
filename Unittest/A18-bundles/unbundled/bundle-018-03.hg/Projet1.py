1d18a5ee4298726509e6f0f8e4cc2e75c4df6ecb
Pierre Snell <pierre.snell.1@ulaval.ca>
2018-11-06 15:20:57 -0500

    Final

--- Unittest/A18-bundles/unbundled/bundle-018-03.hg/Projet1.py
+++
@@ -1,8 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", dest='DATE', help="debut de la date")
-parser.add_argument("-f", dest='DATE', help="fin de la date")
-parser.add_argument("-v", dest='{fermeture,ouverture,min,max,volume}', help="valeur boursiere")
-parser.add_argument("symbole", help="Nom du symbole boursiere")
-args = parser.parse_args
-
